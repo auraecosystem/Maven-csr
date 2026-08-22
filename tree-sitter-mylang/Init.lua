986ccb396ee73b7bae83a5d6ecd290d764837acc
Seriki Walter Yakub <251400999+auraecosystem@users.noreply.github.com>
2026-08-22 04:25:33 +0100

    Add Treesitter configuration for textobjects

---
+++ tree-sitter-mylang/Init.lua
@@ -0,0 +1,15 @@
+require'nvim-treesitter.configs'.setup {
+  textobjects = {
+    select = {
+      enable = true,
+      keymaps = {
+        ["af"] = "@function.outer",
+        ["if"] = "@function.inner",
+        ["ac"] = "@class.outer",
+        ["ic"] = "@class.inner",
+        ["aa"] = "@parameter.outer",
+        ["ia"] = "@parameter.inner",
+      },
+    },
+  },
+}
